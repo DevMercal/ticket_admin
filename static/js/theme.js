1cc6f081367b9b2aa56628ef5bed1a8e9cba517b
Luis Navarro <lm395746@gmail.com>
2025-10-29 14:33:00 -0400

    areglo de l problema de cambio de thema

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,23 +1,27 @@
-// Función para alternar entre modo claro y oscuro
-function toggleDarkMode() {
-    document.documentElement.classList.toggle('dark');
-    // **Guarda el estado actual del modo oscuro ('true' o 'false')**
-    localStorage.setItem('darkMode', document.documentElement.classList.contains('dark').toString());
-}
+  function toggleDarkMode() {
+            document.documentElement.classList.toggle('dark');
+            localStorage.setItem('darkMode', document.documentElement.classList.contains('dark'));
+        }
 
 // Obtener la preferencia guardada
 const storedDarkMode = localStorage.getItem('darkMode');
 
 // Comprobar preferencias del usuario al cargar la página
-if (storedDarkMode === 'true') {
-    // Si la preferencia guardada es 'true', aplica el modo oscuro
-    document.documentElement.classList.add('dark');
-} else if (storedDarkMode === 'false') {
-    // Si la preferencia guardada es 'false', asegúrate de que no tenga el modo oscuro (modo claro)
-    document.documentElement.classList.remove('dark');
-} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    // **SOLO si NO hay preferencia guardada (primera visita),**
-    // comprueba la preferencia del sistema y aplica si es oscuro
-    document.documentElement.classList.add('dark');
-}
+        if (localStorage.getItem('darkMode') === 'true' || 
+            (!localStorage.getItem('darkMode') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        }
 
+ tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        dark: {
+                            100: '#1E293B',
+                            200: '#0F172A',
+                        }
+                    }
+                }
+            }
+        }
